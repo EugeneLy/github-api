491a4c83010a0d6ad5d20b9b08e9e2f7cf378275
Eugene Lytvynenko <johnnypeaceful9@gmail.com>
2018-07-09 16:05:53 +0300

    disabled dev tools

--- src/configure-store.js
+++ src/configure-store.js
@@ -4,9 +4,9 @@ import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
 const enhancer = compose(
-  applyMiddleware(reduxThunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__
-  && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(reduxThunk)
+  /* window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__() */
 );
 
 const configureStore = (preloadedState) => {
